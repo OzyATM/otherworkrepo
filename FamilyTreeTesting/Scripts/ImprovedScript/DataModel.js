be6487bd06c615277410cfe9097d3568058c61cb
Tony Han <ozyatm@gmail.com>
2017-08-20 18:53:41 -0700

    File re structured

---
+++ FamilyTreeTesting/Scripts/ImprovedScript/DataModel.js
@@ -0,0 +1,54 @@
+﻿var goObject = go.GraphObject.make;
+// Initialize an empty datamodel
+var dataModel = {};
+
+//*********************************************
+// Data Model initialization
+//*********************************************
+function initializeDataModel() {
+    dataModel.nodeArray = [
+        {
+            key: "1",
+            figure: "Circle"
+        },
+        {
+            key: "2",
+            figure: "Circle"
+        },
+        {
+            key: "3",
+            figure: "Square"
+        },
+        {
+            key: "4",
+            figure: "Square"
+        }
+    ];
+
+    dataModel.linkArray = [
+        {
+            from: "1", to: "2"
+        }
+    ];
+}
+
+//***********************************************
+// Generate go data model from our own data model
+//***********************************************
+function generateGoModel(inputModel) {
+    var model = goObject(go.GraphLinksModel);
+    model.nodeDataArray = inputModel.nodeArray.slice(0);
+    model.linkDataArray = inputModel.linkArray.slice(0);
+    return model;
+}
+
+//*********************************************
+// Helper functions
+//*********************************************
+function findCurrentIndex(inputKey) {
+    dataModel.nodeArray.forEach(function (obj, index) {
+        if (obj.key === inputKey)
+            tempIndex = index
+    });
+    return tempIndex;
+}
--- FamilyTreeTesting/Scripts/ImprovedScript/FamilyTreeMain.js
+++ FamilyTreeTesting/Scripts/ImprovedScript/FamilyTreeMain.js
@@ -1,109 +1,12 @@
 ﻿var goObject = go.GraphObject.make;
-
 var mainDiagram
-
-var dataModel = {};
-
-dataModel.nodeArray = [
-    {
-        key: "1",
-        figure: "Circle"
-    },
-    {
-        key: "2",
-        figure: "Circle"
-    },
-    {
-        key: "3",
-        figure: "Square"
-    },
-    {
-        key: "4",
-        figure: "Square"
-    }
-];
-
-dataModel.linkArray = [
-    {
-        from: "1", to: "2"
-    }
-];
-
-function generateGoModel(inputModel) {
-    var model = goObject(go.GraphLinksModel);
-    model.nodeDataArray = inputModel.nodeArray.slice(0);
-    model.linkDataArray = inputModel.linkArray.slice(0);
-    return model;
-}
-
-
-//*********************************************
-// Node Definition
-//*********************************************
-
-var adm =
-    goObject(
-        go.Adornment,
-        "Auto",
-        goObject(
-            go.Shape,
-            "Rectangle",
-            {
-                fill: null,
-                stroke: "blue",
-                strokeWidth: 3
-            }
-        ),
-        goObject(
-            go.Panel,
-            "Auto",
-            goObject(go.Placeholder)
-        ),
-        goObject(
-            go.Panel,
-            "Horizontal",
-            {
-                alignment: go.Spot.Bottom,
-                alignmentFocus: go.Spot.Bottom
-            },
-            createDeleteBtn(deleteNode, "刪除", 50)
-        )
-    )
-
-function getNodeTemplate(mainShape, adornment, text) {
-    var personNodeTemplate = goObject(
-        go.Node,
-        "Auto", // Alignment setting is not used, we manually set item position
-        goObject(
-            go.Shape,
-            {
-                width: 30,
-                height: 30,
-                strokeWidth: 5,
-                maxSize: new go.Size(40, 40),
-                cursor: "pointer",
-                fill: "white"
-            },
-            new go.Binding("figure")
-        ),
-        goObject(
-            go.TextBlock,
-            {
-                margin: 5
-            },
-            new go.Binding("text", "displayText")
-        ),
-        {
-            selectionAdornmentTemplate: adm
-        }
-    );
-    return personNodeTemplate;
-}
 
 //*********************************************
 // Graph initialization
 //*********************************************
 function initializeDiagram() {
+    // Initialize Our DataModel, in the future, support load from file
+    initializeDataModel();
     mainDiagram = goObject(
         go.Diagram, // go Type
         "mainDiagramDiv", // div id
@@ -112,50 +15,6 @@ function initializeDiagram() {
             "undoManager.isEnabled": false
         }
     )
-    mainDiagram.nodeTemplate = getNodeTemplate();
+    mainDiagram.nodeTemplate = generateNodeTemplate();
     mainDiagram.model = generateGoModel(dataModel);
-}
-
-function createDeleteBtn(event, btnText, btnWidth) {
-    var deleteBtn;
-    var inputEvent = event;
-    var inputText = btnText;
-    var inputWidth = btnWidth;
-    deleteBtn =
-        goObject(
-            "Button",
-            {
-                margin: 5,
-                width: inputWidth,
-                height: 30,
-                "ButtonBorder.fill": "#FF2D2D",
-                "ButtonBorder.stroke": null,
-                "ButtonBorder.figure": "RoundedRectangle",
-                "_buttonFillOver": "#FF2D2D",
-                "_buttonStrokeOver": null,
-                click: inputEvent
-            },
-            goObject(
-                go.TextBlock,
-                inputText,
-                { font: "10pt sans-serif ", stroke: "white" }
-            )
-        )
-    return deleteBtn
-}
-
-function deleteNode(e, object) {
-    var node = object.part.adornedPart;
-    var nodeKey = node.data.key;
-    var objIndex = findCurrentIndex(nodeKey);
-    dataModel.nodeArray.splice(objIndex, 1);
-    mainDiagram.model = generateGoModel(dataModel);
-}
-
-function findCurrentIndex(inputKey) {
-    dataModel.nodeArray.forEach(function (obj, index) {
-        if (obj.key === inputKey)
-            tempIndex = index
-    });
-    return tempIndex;
 }
